66a151258639d0d986735bf42e2128295e99e3ed
Maya Blitz <mblitz@travelers.com>
2023-02-02 14:49:25 -0500

    added routing and homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import {Films} from './components/Films.js';
+import { Films } from './components/Films.js';
 import Planets from './components/Planets.js';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Films />
-      {/* <Planets /> */}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>&nbsp;
+          <Link to="/films">Films</Link>&nbsp;
+          <Link to="/planets">Planets</Link>&nbsp;
+        </nav>
+        <Routes>
+          <Route path='films' element={<Films />} />
+          {/* <Route path='planets' element={<Planets />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
